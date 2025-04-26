354e48055c91b689a646d2c0ffcbaf64255ff424
Dasun Dilshan <164143926+kmddkuruppu@users.noreply.github.com>
2025-04-26 10:19:21 +0530

    Backend

---
+++ sslp-backend/src/test/java/net/javaguides/sslp/SslpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.sslp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
