045c68a1ddb6f60762b0f2026b1fdd49bbff36fe
Dasun Dilshan <164143926+kmddkuruppu@users.noreply.github.com>
2025-04-27 10:56:20 +0530

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Footer from './Components/Footer';
 
 //import User pages
 import Home from './User/Home';
+import About from './User/About';
+import Contact from './User/Contact';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
