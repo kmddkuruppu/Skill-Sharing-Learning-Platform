1798a0181fe9f61cc37782b5a9fda62e4b3f887b
Dasun Dilshan <164143926+kmddkuruppu@users.noreply.github.com>
2025-04-29 09:09:43 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,17 +10,26 @@ import Contact from './User/Contact';
 import Learning from './User/Learning';
 import Course from './User/Course'
 
+//import Admin pages
+import AddCouse from './Admin/AddCouse';
+
 function App() {
   return (
     <div className="App">
       <Router>
       <Nav />
         <Routes>
+          {/* User */}
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path='/learning' element={<Learning />} />
           <Route path='/course' element={<Course />} />
+
+
+
+          {/* Admin */}
+          <Route path='/addcourse' element={<AddCouse />} />
         </Routes>
         <Footer />
       </Router>
