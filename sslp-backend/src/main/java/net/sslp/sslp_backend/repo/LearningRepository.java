cc948dddb32d19691eb5cae85d7328d45ed3980d
Dasun Dilshan <164143926+kmddkuruppu@users.noreply.github.com>
2025-04-30 23:59:53 +0530

    Remove

--- sslp-backend/src/main/java/net/sslp/sslp_backend/repo/LearningRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.sslp.repo;
-
-import net.javaguides.sslp.model.Learning;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LearningRepository extends MongoRepository<Learning, String> {
-}
