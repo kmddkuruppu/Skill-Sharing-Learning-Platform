e3606c7dec5c0e0b49234111fbd769a88e5df48c
Dasun Dilshan <164143926+kmddkuruppu@users.noreply.github.com>
2025-05-01 12:25:29 +0530

    Remove Skill post Backend

--- sslp-backend/src/main/java/net/javaguides/sslp/repo/SkillPostRepository.java
+++
@@ -1,7 +0,0 @@
-package net.javaguides.sslp.repo;
-
-import net.javaguides.sslp.model.SkillPost;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SkillPostRepository extends MongoRepository<SkillPost, String> {
-}
